b67930c4e9155d77128c844a27976d296535ec73
Robb Andrew <robb@procurewizard.com>
2013-10-15 10:01:56 +0100

    Initial Commit

---
+++ TupleExample/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace TupleExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            var tupleThing = new TupleThing();
+            var result = tupleThing.Operations(10, 3);
+
+            Console.WriteLine("Sum: {0}, Subtract:{1}, Multiply: {2}, Division:{3}", result.Item1, result.Item2, result.Item3, result.Item4);
+            Console.ReadLine();
+        }
+    }
+
+    public class TupleThing
+    {
+        public Tuple<int, int, int, double> Operations(int i,int j)
+        {
+            return Tuple.Create(Sum(i,j), Subtract(i, j), Multiply(i,j), Divide(i,j));
+        }
+
+        private static int Sum(int i, int j)
+        {
+            return i + j;
+        }
+
+        private static int Subtract(int i, int j)
+        {
+            return i - j;
+        }
+
+        private static int Multiply(int i, int j)
+        {
+            return i*j;
+        }
+
+        private static double Divide(int i, int j)
+        {
+            return Convert.ToDouble(i)/j;
+        }
+    }
+}
